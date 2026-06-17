6ee9560715d29e8e205b37ece7205110a000888a
anyatomik <anya.tomilova15@gmail.com>
2026-06-17 21:38:56 +0500

    all backend done

--- calc.cpp
+++ calc.cpp
@@ -14,12 +14,44 @@ void Expression::cleanSpaces(){
 }
 bool Expression::allRight(){
 	for (char c : exp ) {
-		if (c  < '0' && c > '9' && c != '+' && c != '-'){
+		if ((c  < '0' ||  c > '9') && ( c != '+' && c != '-')){
 			return false;
 		}
 	}
-	return true;
+	return exp.back() != '+' && exp.back() != '-' && exp[0] != '+' && exp[0] != '-';
 }
-void Exprssion::distribution() {
-	
+void Expression::distribution() {
+	std::string temp = "";
+	for (char c : exp) {
+		if (c >= '0' && c  <= '9') {
+		temp += c;
+		}
+		else if  (c == '+' || c== '-') {
+			numbers.emplace_back(temp);
+			temp.clear();
+			temp += c;
+			signs.emplace_back(temp);
+			temp.clear();
+		}
+	}
+	if (temp != "") {
+		numbers.emplace_back(temp);
+	}
 }
+void Expression::calculate() {
+	cleanSpaces();
+	if (allRight()) {
+		distribution();
+		int siz = signs.size();
+		result = std::stoi(numbers[0]);
+		for (int i = 0; i < siz; i++) {
+			if (signs[i] == '+') {
+				result += std::stoi(numbers[i+1]);
+			}
+			else if (signs[i] == '-') {
+				result -= std::stoi(numbers[i+1]);
+			}
+		} 
+	}
+}
+
